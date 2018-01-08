7766c176b5e4b5209d90e14f5ecfd32545899fea
Problematis <peter.hancock@selestaltd.co.uk>
2018-01-08 08:53:10 +0000

    changes rclone path

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo python /home/pi/dev/speedtestpy/speedtest.py >> /home/pi/dev/speedtestpy/results/speedtest.csv
-rclone copy results remote:rclone
+/usr/local/bin/rclone copy results remote:rclone
