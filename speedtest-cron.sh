c30b2c3349713904df54e066117055035493bba5
Problematis <peter.hancock@selestaltd.co.uk>
2018-01-09 22:01:06 +0000

    added rclone.conf back to path. whole path in quotes

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo python /home/pi/dev/speedtestpy/speedtest.py >> /home/pi/dev/speedtestpy/results/speedtest.csv
-sudo /usr/local/bin/rclone --config=/home/pi/dev/speedtestpy/.config/rclone copy results remote:rclone
+sudo /usr/local/bin/rclone --config="/home/pi/dev/speedtestpy/.config/rclone/rclone.conf" copy results remote:rclone
